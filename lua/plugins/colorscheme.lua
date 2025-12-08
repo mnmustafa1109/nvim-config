efa7e5d2162cd7e061af105f0a23095c2e6648c8
mnmustafa1109 <mnmustafa1109@gmail.com>
2025-12-08 23:03:21 +0500

    updated plugins and colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
